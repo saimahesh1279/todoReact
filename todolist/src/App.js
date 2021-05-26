cbcdbdcd7cb5df2cb2be2eab8124ee6b82218655
saimahesh1279 <saimahesh1279@gmail.com>
2021-05-26 15:12:25 +0530

    1st

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        TodoList / >
+        <
+        /div>
+    );
+}
+
+export default App;
